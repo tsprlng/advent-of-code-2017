4e6a9150f26fed22c334391fa4904983a6e336ac
Tom Spurling <tom@tspurling.co.uk>
2017-12-01 09:23:43 +0000

    Day 1

---
+++ Day1.hs
@@ -0,0 +1,20 @@
+module Day1 where
+import Data.Char (digitToInt, isDigit)
+import Control.Monad (liftM)
+
+valueIfMatching :: Char -> Char -> Int
+valueIfMatching a b =
+  if a == b then digitToInt a else 0
+
+count1 input =
+  sum $ zipWith valueIfMatching input (tail input ++ [head input])
+
+count2 input =
+  sum $ zipWith valueIfMatching input (end ++ start)
+  where
+    (start, end) = splitAt (length input `div` 2) input
+
+main = do
+  input <- liftM (filter isDigit) $ readFile "Day1.txt"
+  putStrLn . show $ count1 input
+  putStrLn . show $ count2 input
