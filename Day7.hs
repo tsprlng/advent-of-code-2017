436f835de4d4bbd7bd04f94b97ab45146f85965e
Tom Spurling <tom@tspurling.co.uk>
2017-12-07 23:24:46 +0000

    Slightly higher chance of Day7 being readable now

--- Day7.hs
+++ Day7.hs
@@ -3,7 +3,7 @@ module Day7 where
 import Text.ParserCombinators.Parsec
 import qualified Data.Map as M
 import Data.Either (either)
-import Data.Maybe (isNothing, isJust, fromJust)
+import Data.Maybe (isNothing, isJust, fromJust, fromMaybe)
 import Data.List (find, group, sort, groupBy, sortOn)
 import Control.Exception (throw)
 import Control.Monad (liftM)
@@ -31,29 +31,33 @@ absorbErrors = map (either (error.show) id)
 
 
 toTree :: [Program] -> (Prog, Collector)
-toTree inputs = (maybe (error "wat") id $ find (\(_,_,parent,_)-> isNothing parent) $ map snd $ M.toList finalMap, finalMap)
+toTree inputs = (root, finalMap)
   where
-    finalMap = foldl upd M.empty inputs
+    root = fromJust $ find (\(_,_,parent,_)-> isNothing parent) $ map snd $ M.toList finalMap
+    finalMap = foldl update M.empty inputs
       where
-        upd :: Collector -> (Name, Weight, [Name]) -> Collector
-        upd map (name, weight, children) = foldl upChild (upSelf map) children
+        update :: Collector -> (Name, Weight, [Name]) -> Collector
+        update map (name, weight, children) = foldl updateChild (updateSelf map) children
           where
-            upChild :: Collector -> Name -> Collector
-            upChild mp nm = M.alter (Just.(\(_,w,p,c)->(nm,w,Just name,c)).(maybe (def nm) id)) nm mp
-            upSelf :: Collector -> Collector
-            upSelf = M.alter (Just.(\(_,_,p,_)->(name, weight, p, children)).(maybe (def name) id)) name
-            def name = (name, -1, Nothing, [])
+            updateSelf :: Collector -> Collector
+            updateSelf = M.alter ((\(_,_,p,_)-> Just (name, weight, p, children)) . entryOrDefault name) name
+            updateChild :: Collector -> Name -> Collector
+            updateChild map nm = M.alter ((\(n,w,_,cs)-> Just (n, w, Just name, cs)) . entryOrDefault name) nm map
+            entryOrDefault name = fromMaybe (name, -1, Nothing, [])
 
-type Answer2 = (Weight, Maybe [(Weight,Prog)])
-findWrong :: (Prog, Collector)->Answer2
-findWrong (root@(_,weight,_,children), mapp) =
-  ((weight+).sum $ map fst nexts, maybe isItMe id $ find isJust $ map snd nexts)
-    where
-      nexts :: [Answer2]
-      nexts = map (\cName-> maybe (0, Nothing) (\c->findWrong (c,mapp)) $ M.lookup cName mapp) children
-      isItMe
-        | 2 > (length $ group $ map fst nexts) = Nothing
-        | otherwise = Just $ zip (map fst nexts) $ map (\cName -> maybe (error "waat") id $ M.lookup cName mapp) children
+
+type Answer2 = (Weight, Maybe [(Weight,Prog)])  -- total weight, maybe disagreeing nodes and their subtree weights
+
+findWrong :: (Prog, Collector) -> Answer2
+findWrong (root@(_,weight,_,children), mapp) = (totalWeight, anyoneToBlame)
+  where
+    totalWeight = (+ weight) $ sum $ map fst nexts
+    anyoneToBlame = fromMaybe isItOneOfMine $ find isJust $ map snd nexts
+    nexts :: [Answer2]
+    nexts = map (\cName-> maybe (0, Nothing) (\c->findWrong (c,mapp)) $ M.lookup cName mapp) children
+    isItOneOfMine
+      | (<=1) (length $ group $ map fst nexts) = Nothing  -- no, they're all the same
+      | otherwise = Just $ zip (map fst nexts) $ map (\cName -> fromJust $ M.lookup cName mapp) children
 
 answerTheQuestion :: [(Weight, Prog)] -> Weight
 answerTheQuestion input =
@@ -62,6 +66,7 @@ answerTheQuestion input =
       (wrongWeight, (_,wrongProgWeight,_,_)):_ = wrong
       (rightWeight, _):_ = right
       wrong:right:_ = sortOn length $ groupBy (\a b -> fst a == fst b) $ sort input
+
 
 main = do
   input <- liftM (absorbErrors . map parseLine . lines) $ readFile "Day7.txt" :: IO [Program]
