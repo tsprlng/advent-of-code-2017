002657861bec51630ed91b34b63ba7a502d9d172
Tom Spurling <tom@tspurling.co.uk>
2017-12-07 23:47:22 +0000

    Make Day6 a bit more readable hopefully...

--- Day6.hs
+++ Day6.hs
@@ -1,28 +1,31 @@
-module Day6 where
+module Main where
 import Data.List (elemIndex)
 import Data.Maybe (fromJust)
-import Debug.Trace (trace)
 import Control.Monad (liftM)
 
-rotate n xs = zipWith const (drop n (cycle xs)) xs
-
-redist dist = map newBlocks $ zip3 [0..] (rotate (leng-chosen-1) [1..leng]) dist
+rotate n xs = end ++ start
   where
-    newBlocks x@(idx, over, oldBlocks) = {-trace x $-} evens + (if idx==chosen then 0 else oldBlocks) + (if over<=left then 1 else 0)
-    evens = div pile leng
-    leng = length dist
-    pile = maximum dist
-    left = mod pile leng
-    chosen = fromJust $ elemIndex pile dist
-    --trace' = trace ("evens " ++ show evens ++ "  leng " ++ show leng ++ "  pile " ++ show pile ++ "  left " ++ show left ++ "  chosen " ++ show chosen)
+    (start, end) = splitAt n xs
 
-answer1 input = ans [] input
+findMaximum xs = findMax (-1,0) 0 xs
+  where
+    findMax ans _ [] = ans
+    findMax ans@(lastPos, bestSoFar) myPos (x:xs) = findMax (if x > bestSoFar then (myPos, x) else ans) (myPos+1) xs
+
+redist dist = map newBlocks $ zip3 [0..] (rotate (len-position-1) [1..len]) dist
+  where
+    len = length dist
+    newBlocks x@(idx, order, oldBlocks) = evens + (if idx==position then 0 else oldBlocks) + (if order<=leftOver then 1 else 0)
+    (evens, leftOver) = divMod toRedist len
+    (position, toRedist) = findMaximum dist
+
+answer1 input = ans [] input  -- how long until pattern starts repeating?
   where
     ans pastDists currentDist
       | elem currentDist pastDists = 0
       | otherwise = 1 + ans (currentDist:pastDists) (redist currentDist)
 
-answer2 input = ans [] input
+answer2 input = ans [] input  -- how long is loop?
   where
     ans pastDists currentDist = maybe next (1+) $ elemIndex currentDist pastDists
       where
