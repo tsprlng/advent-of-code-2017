4a99c8950c52912b4dda4092b28a3ab04750a563
Tom Spurling <tom@tspurling.co.uk>
2017-12-09 13:38:40 +0000

    Day9: Becoming slightly more adept with Parsec
    ...and it's starting to pay off

---
+++ Day9.hs
@@ -0,0 +1,43 @@
+module Day9 where
+
+import Text.ParserCombinators.Parsec
+
+data Stuff = Group [Stuff] | Garbage String
+  deriving (Eq, Show)
+
+stuffParser :: GenParser Char st Stuff
+stuffParser = garbage <|> group
+  where
+    group =
+      do
+        string "{"
+        things <- sepBy stuffParser (string ",")
+        string "}"
+        return $ Group things
+    garbage =
+      do
+        string "<"
+        content <- many (ignore <|> ((:[]) <$> noneOf ">"))
+          -- TODO could filter Maybes or something instead (but [] seems more or less equivalent?)
+        string ">"
+        return $ Garbage $ concat content
+
+    ignore = char '!' >> anyChar >> return ""
+
+parseStuff = either (error.show) id <$> parse stuffParser ""
+
+score :: Stuff -> Int
+score = score' 1
+  where
+    score' accum (Group xs) = (accum +) $ sum (map (score' (accum+1)) xs)
+    score' _ (Garbage _) = 0
+
+countGarbage :: Stuff -> Int
+countGarbage (Group xs) = sum $ map countGarbage xs
+countGarbage (Garbage s) = length s
+
+
+main = do
+  input <- parseStuff <$> readFile "Day9.txt" :: IO Stuff
+  putStrLn . show $ score input
+  putStrLn . show $ countGarbage input
